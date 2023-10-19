c688fe8943d744bc9d2cafec9155521cdba12eb2
namalwamillia <145693220+namalwamillia@users.noreply.github.com>
2023-10-19 10:24:39 +0300

    Add files via upload

---
+++ Python_Test_one/question_one.py
@@ -0,0 +1,13 @@
+#(a)snake case
+#student_marks
+
+#(b)camel case
+#totalAverage
+
+#(ii)
+age=22
+print(float(22))
+print(type(22))
+
+
+
---
+++ Python_Test_one/question_three.py
@@ -0,0 +1,22 @@
+#let x be list
+#(i)
+x=[78,83,90,88,75]
+print(sum(x))
+#(ii)
+#sutends mark =[78,83,90,88,75]
+for x in [78,83,90,88,75]:
+    if x==",83,99,88":
+     break
+    print(x)
+
+
+
+
+#(iv)
+for x in range(78,87):
+   print(x)
+   #(iii)
+  # add 96 in list
+  let x =96
+list=[78,83,90,88,75,x]
+print(list)
